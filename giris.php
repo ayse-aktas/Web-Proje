0fa6c383fda71ad4ec221372e8b8c667ca344939
Ayse Aktas <ayse.aktas8@ogr.sakarya.edu.tr>
2023-05-19 01:15:01 +0300

    Web sitesi hazır.

--- giris.php
+++ giris.php
@@ -12,17 +12,17 @@ if(!($_POST['mail']==""))
         if(!($_POST['sifre']=="221210036"))
         {
             echo "Şifre hatalı girilmiştir.";
-            echo "<p><a href='Giris.html'>&lt;GERİ DÖN&gt;></a></p> ";
+            echo "<p><a href='index.html'>&lt;GERİ DÖN&gt;></a></p> ";
         }
         else{
             echo "<script type='text/javascript'>alert('$message');</script>";
-            header("Refresh: 0.1; url=http://localhost//ayse_aktas_web_proje/Hakkinda.html"); 
+            header("Refresh: 0.1; url=Hakkinda.html"); 
             exit();
         }
     }
     else{
         echo "E-posta hatalı girilmiştir.";
-        echo "<p> <a href='Giris.html'>&lt;GERİ DÖN&gt;</a></p> ";
+        echo "<p> <a href='index.html'>&lt;GERİ DÖN&gt;</a></p> ";
     }
 }else
 {
